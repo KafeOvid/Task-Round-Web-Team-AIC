90ff8665ac6d27bdba2b39996d7469e790c3ee72
KafeOvid <ayushmanng04@gmail.com>
2024-10-22 19:11:03 +0530

    Update Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 // src/App.jsx
 import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Events from './components/Events';
-import Footer from './components/Footer';
-import './App.css'; // Main CSS file for overall styling
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import Events from './Components/Events';
+import Footer from './Components/Footer';
 
 const App = () => {
     return (
